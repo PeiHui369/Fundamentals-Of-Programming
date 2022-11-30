d004f48d8126496a71d8503c23e1e62d3565142f
PeiHui369 <108777577+PeiHui369@users.noreply.github.com>
2022-11-30 14:58:47 +0800

    Update L5Q6(qta).java

--- Lab 5/L5Q6(qta).java
+++ Lab 5/L5Q6(qta).java
@@ -43,3 +43,28 @@ public class L5Q6 {
         keyboard.close();
     }
 }
+
+
+>>
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter the row of Pascal's triangle to generate: ");
+    int N = in.nextInt();
+    int[][] tri = new int [N][N];
+
+    tri[0][0] = 1;
+    for(int i = 1 ; i<N ; i++){
+        tri[1][0] = 1;
+        for(int j = 1; j<i; j++){
+            tri[i][j] = tri[i-1][j] + tri [i-1][j-1];
+    }
+}
+    
+    // all will get 0
+    for(int i = 0; i < N; i++){
+        for(int j= 0； j<N; j++){
+            System.out.printf("%d ", tri[i][j]);
+        }
+        System.out.println("");
+    }
+    
+  
