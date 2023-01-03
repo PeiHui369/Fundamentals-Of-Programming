b09dcad78c9b2b1c8ca78fa6ec911227cdeb46c5
PeiHui369 <108777577+PeiHui369@users.noreply.github.com>
2023-01-03 22:07:18 +0800

    Update and rename L5Q5(qta).java to L5Q5.java

--- Lab 5/L5Q5.java
+++ Lab 5/L5Q5.java
@@ -1,3 +1,84 @@
+<FINAL ANS>
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[20];
+        int search = 0;
+
+        System.out.println("A list of 20 random integer within 0 to 100");
+
+        for (int i = 0; i < 20; i++) {
+            arr[i] = (int) (Math.random() * 100);
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        reverse(arr);
+
+        System.out.print("Enter a number to search: ");
+        search = sc.nextInt();
+
+        linearSearch(arr, search);
+        binarySearch(arr, search);
+
+        sc.close();
+    }
+
+    static void reverse(int[] array) {
+
+        int n = array.length;
+
+        System.out.println("Array in descending order");
+
+        Arrays.sort(array);
+
+        for (int j = 0; j < n / 2; j++) {
+            int temp = array[j];
+            array[j] = array[(n - 1) - j];
+            array[(n - 1) - j] = temp;
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    static void linearSearch(int[] array, int n) {
+
+        int loopCount = 0;
+
+        for (int i: array) {
+            if (i == n) { break; }
+            loopCount++;
+        }
+
+        if (loopCount < array.length) {
+            System.out.printf("%d found\nLinear Search - %d loop(s)\n", n, loopCount);
+        }else{
+            System.out.println("Not found");
+        }
+    }
+
+    static void binarySearch(int[] array, int n) {
+        int low = 0, high = array.length-1, mid = 0, loop=0;
+        while (low < high){
+            mid = (low + high)/2;
+            if( n == array[mid]){System.out.println(mid);}
+            else if( n > array[mid]){low = mid+1; } //n is on right side
+            else {high = mid -1;} //n is on left side
+        loop++;
+        }
+        if(mid == n){
+        System.out.println("Binary Search - "+loop+" loop(s)");}
+        else{System.out.println("Not found");}
+    }
+}
+
+=================================================================
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
