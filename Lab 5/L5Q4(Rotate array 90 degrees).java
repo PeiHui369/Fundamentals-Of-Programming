3ceb3b78e115eba4674bfe97f9d65238e6879751
PeiHui369 <108777577+PeiHui369@users.noreply.github.com>
2023-01-11 09:33:35 +0800

    Update L5Q4(Rotate array 90 degrees).java

--- Lab 5/L5Q4(Rotate array 90 degrees).java
+++ Lab 5/L5Q4(Rotate array 90 degrees).java
@@ -5,16 +5,16 @@ public class Main {
         int[][] matrix = {{1,5,7},{3,6,9},{5,3,8}};
         int[][] matrix2 = new int [matrix.length][matrix[0].length];
         
-        for(int i = 0, j = matrix.length-1; i < matrix.length; i++, j--) {
-                matrix2[0][j] = matrix[i][0];
-                matrix2[1][j] = matrix[i][1];
-                matrix2[2][j] = matrix[i][2];
+        for(int i = 0, j = matrix.length-1; i < matrix.length; i++, j--) {  #
+                matrix2[0][j] = matrix[i][0];                               #
+                matrix2[1][j] = matrix[i][1];                               #
+                matrix2[2][j] = matrix[i][2];                               #
             }
         
         for(int i = 0; i <= matrix.length-1; i++){
             for(int j = 0; j <= matrix[0].length-1; j++){
                 System.out.print(matrix[i][j] + " ");
-                if(j>0 && j%2 == 0){System.out.print("\n");}
+                if(j>0 && j%2 == 0){System.out.print("\n");}                # To print next line in matrix
             }
         }
 
