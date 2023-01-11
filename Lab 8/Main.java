da2a6fd215ff0a3551c1e43bba7783c9777f604b
PeiHui369 <108777577+PeiHui369@users.noreply.github.com>
2023-01-11 13:28:08 +0800

    Add files via upload

---
+++ Lab 8/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the type of durian [Quit] to terminate: ");
+        String type = in.nextLine();
+        System.out.println("Enter the sales in kg: ");
+        int sales = in.nextInt();
+        double sales = 0;
+
+        while (type != "QUIT"){
+
+        }
+
+        System.out.println("Total sales: ")
+    }
+}
